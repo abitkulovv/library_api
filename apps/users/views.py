cd4d78673477d92d4ba423f090f0810d1d28324f
Dastan <dastanabitkulov901@gmail.com>
2026-03-03 17:06:06 +0600

    fix: update README, add Dockerfile, fix duplicate import

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework import status, permissions
 
 from apps.users.serializers import UserRegisterSerializer, UserSerializer
 from apps.users.models import User
